2a187840335e48a38e9c2aec3919431abc1f2a52
Vladimir Ilimurzin <ilimurzin@ya.ru>
2023-12-02 12:23:53 +0300

    feat: Применил JavaScript Standard Style, запустив `npx standard --fix`

--- install/js/script.js
+++ install/js/script.js
@@ -6,42 +6,42 @@ BX.addCustomEvent('OnEditorInitedBefore', function (editor) {
     toolbarSort: 135,
     src: '/bitrix/images/ilimurzin.typograf/icon.svg',
     handler: () => {
-      const typograf = new Typograf({locale: ['ru', 'en-US']});
+      const typograf = new Typograf({ locale: ['ru', 'en-US'] })
 
-      typograf.addSafeTag('<\\?', '\\?>');
-      typograf.addSafeTag('\\[code\\]', '\\[/code\\]');
-      typograf.addSafeTag('\\[CODE\\]', '\\[/CODE\\]');
-      typograf.addSafeTag('\\[quote\\]', '\\[/quote\\]');
-      typograf.addSafeTag('\\[QUOTE\\]', '\\[/QUOTE\\]');
+      typograf.addSafeTag('<\\?', '\\?>')
+      typograf.addSafeTag('\\[code\\]', '\\[/code\\]')
+      typograf.addSafeTag('\\[CODE\\]', '\\[/CODE\\]')
+      typograf.addSafeTag('\\[quote\\]', '\\[/quote\\]')
+      typograf.addSafeTag('\\[QUOTE\\]', '\\[/QUOTE\\]')
 
-      const wait = BX.showWait(editor.dom.areaCont);
+      const wait = BX.showWait(editor.dom.areaCont)
 
-      const content = editor.GetContent();
-      const range = editor.selection.GetRange();
+      const content = editor.GetContent()
+      const range = editor.selection.GetRange()
 
-      let contentBefore;
-      let contentAfter;
-      let result;
+      let contentBefore
+      let contentAfter
+      let result
 
       if (range.startOffset === range.endOffset) {
-        contentBefore = '';
-        contentAfter = '';
-        result = content;
+        contentBefore = ''
+        contentAfter = ''
+        result = content
       } else {
-        contentBefore = content.substring(0, range.startOffset);
-        contentAfter = content.substring(range.endOffset);
-        result = content.substring(range.startOffset, range.endOffset);
+        contentBefore = content.substring(0, range.startOffset)
+        contentAfter = content.substring(range.endOffset)
+        result = content.substring(range.startOffset, range.endOffset)
 
         typograf.disableRule('common/space/trimLeft')
         typograf.disableRule('common/space/trimRight')
       }
 
-      result = typograf.execute(result);
+      result = typograf.execute(result)
 
-      editor.SetContent(contentBefore + result + contentAfter, true);
-      editor.ReInitIframe();
+      editor.SetContent(contentBefore + result + contentAfter, true)
+      editor.ReInitIframe()
 
-      BX.closeWait(wait);
+      BX.closeWait(wait)
     }
-  });
-});
+  })
+})
